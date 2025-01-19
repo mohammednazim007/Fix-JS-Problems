38e0f60770f34948d8d4297e1db2429e3df0c8ef
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-19 10:14:21 +0600

    get last elements function created successfully

--- index.js
+++ index.js
@@ -390,4 +390,5 @@ const getLastElement = (arr, n) => {
 };
 
 const firstArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(getFirstElement(firstArray, 8));
+// console.log(getFirstElement(firstArray, 8));
+// console.log(getLastElement(firstArray,3));
