1a0590bbcf9556bd221bf48c7cd02a5f749abd5e
mohammednazim007 <mohammednazim3629@gmail.com>
2025-01-30 10:34:07 +0600

    add function to generate all permutations of an array's elements

--- index.js
+++ index.js
@@ -759,3 +759,24 @@ const createObject = (array, key) => {
 //     "id"
 //   )
 // );
+
+// 082: Write a JavaScript program to generate all permutations of an array's elements (including duplicates).
+const permutationsArray = (array) => {
+  let result = [];
+
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m);
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next));
+      }
+    }
+  };
+
+  permute(array);
+  return result;
+};
+// console.log(permutationsArray([2,4]));
