7f47ae5f1efc355e1e3640516e5df005ac89276e
mohammednazim007 <mohammednazim3629@gmail.com>
2025-02-02 17:10:26 +0600

    add bubble sort algorithm implementation

--- object-problems.js
+++ object-problems.js
@@ -18,5 +18,23 @@ const deleteProperty = (obj, prop) => {
 // 003: Write a JavaScript program to get the length of a JavaScript object.
 // var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
 // Output: 3
-const getLength=obj=>Object.keys(obj).length
+const getLength = (obj) => Object.keys(obj).length;
 // console.log(getLength({ name: "David Rayy", sclass: "VI", rollno: 12 }));
+
+// Write a bubble sort algorithm in JavaScript.
+// Note : Bubble sort is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted,
+// Sample Data: [6,4,0, 3,-2,1]
+// Expected Output: [-2, 0, 1, 3, 4, 6]
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let w = 0; w < arr.length; w++) {
+      if (arr[w] > arr[w + 1]) {
+        let temp = arr[w];
+        arr[w] = arr[w + 1];
+        arr[w + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+// console.log(bubbleSort([6, 4, 0, 3, -2, 1]));
