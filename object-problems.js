02b67fab17dd9efcc4debdb11ab466e1b313a854
mohammednazim007 <mohammednazim3629@gmail.com>
2025-02-02 16:56:49 +0600

    add function to delete a property from an object

--- object-problems.js
+++ object-problems.js
@@ -5,3 +5,12 @@ function listProperties(obj) {
   return Object.keys(obj).join(", ");
 }
 // console.log(listProperties({ red: "#FF0000", green: "#00FF00", white: "#FFFFFF" }));
+
+// 002: Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+// var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+// Output: { name: "David Rayy", sclass: "VI" }
+const deleteProperty = (obj, prop) => {
+  delete obj[prop];
+  return obj;
+};
+console.log(deleteProperty({ name: "David Rayy", sclass: "VI", rollno: 12 }, "rollno"));
