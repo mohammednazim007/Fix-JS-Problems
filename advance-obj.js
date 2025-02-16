053e2dc6c4bfc420e0fe3e473fcc2e58910b2bfc
mohammednazim007 <mohammednazim3629@gmail.com>
2025-02-16 14:31:06 +0600

    update comments and add examples for Object.preventExtensions and Object.seal in advance-obj.js

--- advance-obj.js
+++ advance-obj.js
@@ -8,9 +8,23 @@ const person = {
   },
 };
 
-// object.preventExtensions - is not allow to add new properties but can modify existing properties
+// Object.preventExtensions - is not allow to add new properties but can modify existing properties
 const modify = Object.preventExtensions(person);
 person.profession = "developer"; // not allowed
-person.age = 24;// allowed
-
+person.age = 24; // allowed
 console.log(person);
+
+const person2 = {
+  name: "Rahul",
+  age: 23,
+  hobbies: ["cricket", "football", "chess"],
+  address: {
+    city: "Mumbai",
+    pincode: 400012,
+  },
+};
+// Object.seal - is not allow to add new properties and also not allowed to delete existing properties but can modify existing properties
+const modify2 = Object.seal(person2);
+person2.profession = "developer"; // not allowed
+delete person2.name; // not allowed
+person2.age = 24; // allowed
