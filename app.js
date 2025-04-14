e1be5d634f94c85c5f8972ae043c6af9fa904402
mohammednazim007 <mohammednazim3629@gmail.com>
2025-04-14 12:25:31 +0600

    feat(app): add random string generator function

---
+++ app.js
@@ -0,0 +1,11 @@
+const randomString = (length) => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+console.log(randomString(10)); // Example usage: generates a random string of length 10
