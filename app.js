e31f54d0f31bf10b1de2d5a6b028dd67dfffec7c
mohammednazim007 <mohammednazim3629@gmail.com>
2025-04-16 16:18:29 +0600

    feat(app): add CORS support and update JSON parsing middleware

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 const db = mongoose.connect("mongodb://localhost/test");
 
-app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
