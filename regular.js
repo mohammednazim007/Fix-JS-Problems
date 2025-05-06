eb012eba450204fe9527146ec61b0f4847271495
mohammednazim007 <mohammednazim3629@gmail.com>
2025-05-06 14:46:33 +0600

    fix: update arrow function to correctly reference 'this' and include fatherName method

--- regular.js
+++ regular.js
@@ -2,5 +2,10 @@ const arrowFn = (name, age) => {
   return {
     name: this.name,
     age: this.age,
+    fatherName: () => {
+      console.log(`Father's name is ${this.name}`);
+    },
   };
 };
+
+const person = arrowFn("John", 30);
