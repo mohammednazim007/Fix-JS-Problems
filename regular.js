37b0c4435ff3f6408229da0b8f28b9fbf7bfa47b
mohammednazim007 <mohammednazim3629@gmail.com>
2025-05-06 14:53:51 +0600

    fix: update motherName structure and correct 'this' reference in printName method

--- regular.js
+++ regular.js
@@ -5,8 +5,15 @@ const arrowFn = function (name, age) {
     fatherName: function () {
       console.log(`Father's name is ${this.name}`);
     },
+    motherName: {
+      name: "Mr. XXX",
+      printName:()=> {
+        console.log(`Mother's name is ${this.name}`);
+      },
+    },
   };
 };
 
 const person = arrowFn("John", 30);
 person.fatherName(); // Father's name is undefined
+person.motherName.printName(); // Mother's name is Mr. XXX
