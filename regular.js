83043848c27308d57eb272ab3af2bbd8ef7e7dc6
mohammednazim007 <mohammednazim3629@gmail.com>
2025-05-06 15:01:39 +0600

    fix: refactor person object structure and update function to log name and age

--- regular.js
+++ regular.js
@@ -1,19 +1,9 @@
-const arrowFn = function (name, age) {
-  return {
-    name: name,
-    age: age,
-    fatherName: function () {
-      console.log(`Father's name is ${this.name}`);
-    },
-    motherName: {
-      name: "Mr. XXX",
-      printName: function () {
-        console.log(`Mother's name is ${this.name}`);
-      },
-    },
-  };
+const person = {
+  name: "Mohammed Nazim",
+  age: 22,
 };
 
-const person = arrowFn("John", 30);
-person.fatherName(); // Father's name is undefined
-person.motherName.printName(); // Mother's name is Mr. XXX
+const personFn = function () {
+  console.log(`Person name is ${person.name} and age is ${person.age}`);
+};
+personFn.call(person);
